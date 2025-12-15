088eed94659664286dc0bde874cc422365343cae
joysondsouza47 <joysondsouza47@gmail.com>
2025-12-15 08:36:19 +0530

    commit 4

--- demo8.java
+++ demo8.java
@@ -1,6 +1,7 @@
 package java_programs;
-public class demo8.java {
-public static void main(String[] args){
-System.out.println("my name is joyson");
-}
+
+public class demo8 {
+  public static void main(String[] args) {
+    System.out.println("my name is joyson");
+  }
 }
